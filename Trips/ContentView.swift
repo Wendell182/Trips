6adc3e6ee5f48313329713f934d05449154fd24d
wendellclaus <wendellclaus@brq.com>
2022-09-27 23:40:22 -0300

    start project SwiftUI

--- Trips/ContentView.swift
+++ Trips/ContentView.swift
@@ -9,8 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { view in
+            //MARK: - VStack Principal
+            VStack {
+                //MARK: - VStack HEader
+                VStack {
+                    Text("Trips")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 20))
+                        .padding(.top, 50)
+                     
+                    Text("ESPECIAL").foregroundColor(Color.white)
+                        .font(.custom("Avenir Book", size: 20))
+                        .frame(minWidth: 0,  maxWidth: .infinity,  alignment: .leading)
+                        .padding(.leading, 30)
+                    Text("BRASIL").foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 23))
+                        .frame(minWidth: 0,  maxWidth: .infinity,  alignment: .leading)
+                        .padding(.leading, 30)
+                }
+                .frame(width: view.size.width, height: 180, alignment: .top)
+                .background(Color.blue)
+                .edgesIgnoringSafeArea(.all)
+                
+                List {
+                    Text("Rio de Janeiro")
+                    Text("Ceará")
+                    Text("Atibaia")
+                    Text("Taubaté")
+                }
+            }
+               
+            }
     }
 }
 
