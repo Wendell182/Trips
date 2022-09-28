f3469348a77e6aef160e56dfdfc424af970bc231
wendellclaus <wendellclaus@brq.com>
2022-09-27 22:11:50 -0300

    Initial Commit

---
+++ Trips/TripsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripsApp.swift
+//  Trips
+//
+//  Created by Wendell Claus on 27/09/22.
+//
+
+import SwiftUI
+
+@main
+struct TripsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
